66daa60bbd85336ac1bb182bf664d6a387b8a0b4
Renan <renancaster@gmail.com>
2018-08-04 00:02:52 -0300

    Commit Projeto Gerenciador de tarefas Angular 6

---
+++ gerenciador-de-tarefas/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'; //modulo de rotas
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
