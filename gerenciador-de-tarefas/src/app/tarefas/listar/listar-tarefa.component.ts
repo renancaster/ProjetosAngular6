71736ee8efd59747f9c751a1ceb3fa1fd60609ae
Renan <renancaster@gmail.com>
2018-08-23 23:33:15 -0300

    Criação remoção de tarefas e alteração de status tarefa

--- gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -21,4 +21,19 @@ export class ListarTarefaComponent implements OnInit {
   listarTodos(): Tarefa[] {
     return this.tarefaService.listarTodos();
   }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault(); //usar esse event para não atualizar a pagina (aqueles refresh)
+    if (confirm('Deseja remover a tarefa' + tarefa.nome + '?')) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa' + tarefa.nome + '?')) {
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
 }
